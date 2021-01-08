c9e492ca78654dd271c286933947c7d923b20d61
Imre, Nagy <imren@hyperborg.com>
2021-01-08 19:35:08 +0100

    Added PHILIPS HUE device

---
+++ plugins/hue/hue_device.cpp
@@ -0,0 +1,357 @@
+#include <hue_device.h>
+
+hue_device::hue_device(QObject *parent) : HDevice(parent)
+{
+}
+
+hue_device::~hue_device()
+{
+}
+
+QJsonObject hue_device::configurationTemplate()
+{
+    QJsonObject obj;
+    return obj;
+}
+
+bool hue_device::loadConfiguration(QJsonObject &json)
+{
+    Q_UNUSED(json);
+    // should have host
+    // should have username
+    return true;
+}
+
+void hue_device::saveConfiguration(QJsonObject &json)
+{
+    Q_UNUSED(json);
+}
+
+void hue_device::init()
+{
+}
+
+void hue_device::setup()
+{
+    netman = new QNetworkAccessManager(this);
+    QObject::connect(netman, SIGNAL(finished(QNetworkReply*)), this, SLOT(handleNetworkData(QNetworkReply*)));
+    QObject::connect(netman,SIGNAL(sslErrors(QNetworkReply*,QList<QSslError>)), this, SLOT(handleSSLErrors(QNetworkReply*)));
+
+    getAllLights();
+    getConfiguration();
+}
+
+void hue_device::connectToRealDevice()
+{
+}
+
+/* ------------ API CMD GENERATION ----------------------- */
+
+// LIGHT API
+void hue_device::getAllLights()
+{
+    proceed(GET, "lights", "", "");
+}
+
+void hue_device::getNewLights()
+{
+    proceed(GET, "lights/new", "", "");
+}
+
+void hue_device::getLightAttributesAndState(QString id)
+{
+    proceed(GET, QString("lights/%1").arg(id), "", "");
+}
+
+void hue_device::setLightAttribute(QString id)  // rename
+{
+    proceed(POST, QString("lights/%1").arg(id), "", "");
+}
+
+void hue_device::setLightState(QString id)
+{
+    proceed(POST, QString("lights/%1/state").arg(id), "", "");
+    // requires body
+}
+
+void hue_device::deleteLight(QString id)
+{
+    proceed(DELETE, QString("lights/%1").arg(id), "", "");
+}
+
+// GROUPS API
+void hue_device::getAllGroups()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::createGroup()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::getGroupAttributes()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::setGroupAttributes()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::setGroupState()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::deleteGroup()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+
+// SCHEDULES API
+void hue_device::getAllSchedules()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::createSchedule()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::getScheduleAttributes()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::setScheduleAttributes()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::deleteSchedule()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+
+// SCENES API
+void hue_device::getAllScenes()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::createScene()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::modifyScene()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::recallScene()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::deleteScene()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::getScene()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+
+// SENSORS API
+void hue_device::getAllSensors()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::createSensor()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::findNewSensors()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::getNewSensors()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::getSensor()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::updateSensor()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::deleteSensor()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::changeSensorState()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+
+// RULES API
+void hue_device::getAllRules()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::getRule()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::createRule()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::updateRule()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::deleteRule()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+// CONFIGURATION API
+void hue_device::createUser()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::getConfiguration()
+{
+    proceed(GET, QString("config"), "", "");
+}
+
+void hue_device::modifyConfiguration()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::deleteUser()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::getFullState()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+// RESOURCELINKS API
+void hue_device::getAllResourceLinks()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::getResourceLinks()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::createResourceLinks()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+void hue_device::updateResourceLinks()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+
+// CAPABILITIES API
+void hue_device::getAllCapabilities()
+{
+    proceed(NOTIMPLEMENTED, QString("/%1").arg(""), "", "");
+}
+
+
+
+
+
+// ================================= COMMON EXECUTION CODE ===================================
+
+void hue_device::handleNetworkData(QNetworkReply *reply)
+{
+    qDebug() << "NETWORK DATA FROM HUE ARRICED: ";
+    if(reply->error() == QNetworkReply::NoError)
+    {
+        QByteArray response = reply->readAll();
+        qDebug() << response;
+    }
+    else // handle error
+    {
+      qDebug() << reply->errorString();
+    }
+    reply->deleteLater();
+}
+
+void hue_device::handleSSLErrors(QNetworkReply *reply)
+{
+    Q_UNUSED(reply);
+}
+
+void hue_device::proceed(hue_device::Mode mode, QString cmd, QString body, QString parser)
+{
+    Q_UNUSED(parser);
+    if (mode==NOTIMPLEMENTED)
+    {
+        qDebug() << "This function is not yet implemented!";
+        return;
+    }
+    QString urlstr = QString("http://%1/api/%2/%3").arg(_host).arg(_username).arg(cmd);
+    QUrl url = QUrl(urlstr);
+    QNetworkRequest request(url);
+
+    request.setRawHeader(QByteArray("User-Agent"), QByteArray("HYPERBORG HUE DEVICE DRIVER"));
+    request.setRawHeader(QByteArray("X-Custom-User-Agent"), QByteArray("HYPERBORG HUE DEVICE DRIVER"));
+    QByteArray bbody = body.toLocal8Bit();
+    QByteArray bsize = QByteArray::number(bbody.size());
+
+    if (bbody.size())
+    {
+        request.setRawHeader("Content-Type", "application/json");
+        request.setRawHeader("Content-Length", bsize);
+    }
+
+    switch(mode)
+    {
+        case GET:
+            netman->get(request);
+            break;
+        case POST:
+            netman->post(request, bbody);
+            break;
+        case DELETE:
+//            netman->deleteResource(request);
+            break;
+    }
+
+}
---
+++ plugins/hue/hue_device.h
@@ -0,0 +1,120 @@
+#ifndef hue_device_H
+#define hue_device_H
+
+#include <QDebug>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QUrl>
+
+#include "hdevice.h"
+
+class hue_device : public HDevice
+{
+    enum Mode
+    {
+        GET,
+        POST,
+        DELETE,
+        NOTIMPLEMENTED
+    };
+
+Q_OBJECT
+public:
+    hue_device(QObject *parent=nullptr);
+    ~hue_device();
+    HyObject::Type type() { return Device; }
+    void disconnect() {}
+    void init();
+
+    QJsonObject configurationTemplate();
+    void saveConfiguration(QJsonObject &json);
+    bool loadConfiguration(QJsonObject &json);
+
+public slots:
+    void setup();
+    void connectToRealDevice(); // creating tcp connection to the actual hardware
+
+protected slots:
+    void handleNetworkData(QNetworkReply *reply);
+    void handleSSLErrors(QNetworkReply *reply);
+
+private slots:
+    // GENERAL
+    void proceed(hue_device::Mode, QString cmd, QString body, QString parser);
+
+    // LIGHTS API
+    void getAllLights();
+    void getNewLights();
+    void getLightAttributesAndState(QString id);
+    void setLightAttribute(QString id);
+    void setLightState(QString id);
+    void deleteLight(QString id);
+
+    // GROUPS API
+    void getAllGroups();
+    void createGroup();
+    void getGroupAttributes();
+    void setGroupAttributes();
+    void setGroupState();
+    void deleteGroup();
+
+    // SCHEDULES API
+    void getAllSchedules();
+    void createSchedule();
+    void getScheduleAttributes();
+    void setScheduleAttributes();
+    void deleteSchedule();
+
+    // SCENES API
+    void getAllScenes();
+    void createScene();
+    void modifyScene();
+    void recallScene();
+    void deleteScene();
+    void getScene();
+    
+    // SENSORS API
+    void getAllSensors();
+    void createSensor();
+    void findNewSensors();
+    void getNewSensors();
+    void getSensor();
+    void updateSensor();
+    void deleteSensor();
+    void changeSensorState();
+    
+    // RULES API
+    void getAllRules();
+    void getRule();
+    void createRule();
+    void updateRule();
+    void deleteRule();
+
+    // CONFIGURATION API
+    void createUser();
+    void getConfiguration();
+    void modifyConfiguration();
+    void deleteUser();
+    void getFullState();
+    
+    // RESOURCELINKS API
+    void getAllResourceLinks();
+    void getResourceLinks();
+    void createResourceLinks();
+    void updateResourceLinks();
+    
+    // CAPABILITIES API
+    void getAllCapabilities();
+    
+
+
+
+private:
+    QNetworkAccessManager *netman;
+
+public: // for testing
+    QString _host;
+    QString _username;
+};
+#endif
