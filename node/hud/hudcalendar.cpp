08d65182d844803c89ed2d953f790007a8a98db2
Imre, NAGY <imi@hyperborg.com>
2022-10-19 20:55:30 +0200

    Added HUDCalendar

---
+++ node/hud/hudcalendar.cpp
@@ -0,0 +1,73 @@
+#include "hudcalendar.h"
+
+//======================================== HUDCalendarEntry =================================================
+HUDCalendarEntry::HUDCalendarEntry(QQuickItem* parent) : HUDElement(parent)
+{
+}
+
+HUDCalendarEntry::~HUDCalendarEntry()
+{
+}
+
+void HUDCalendarEntry::paint(QPainter* painter)
+{
+}
+
+void HUDCalendarEntry::loadConfiguration(QJsonObject& json)
+{
+}
+
+void  HUDCalendarEntry::saveConfiguration(QJsonObject& json)
+{
+}
+
+//======================================== HUDCalendarDay =================================================
+
+HUDCalendarDay::HUDCalendarDay(QQuickItem* parent) : HUDElement(parent)
+{
+}
+
+HUDCalendarDay::~HUDCalendarDay()
+{
+}
+
+void HUDCalendarDay::paint(QPainter* painter)
+{
+}
+
+void HUDCalendarDay::loadConfiguration(QJsonObject& json)
+{
+}
+
+void  HUDCalendarDay::saveConfiguration(QJsonObject& json)
+{
+}
+
+//======================================== HUDCalendar =================================================
+
+HUDCalendar::HUDCalendar(QQuickItem* parent) : HUDElement(parent)
+{
+}
+
+HUDCalendar::~HUDCalendar()
+{
+}
+
+
+void HUDCalendar::loadConfiguration(QJsonObject& json)
+{
+}
+
+void  HUDCalendar::saveConfiguration(QJsonObject& json)
+{
+}
+
+void HUDCalendar::calculateInternalGeometry()
+{
+}
+
+void HUDCalendar::paint(QPainter* painter)
+{
+    
+}
+
---
+++ node/hud/hudcalendar.h
@@ -0,0 +1,85 @@
+#ifndef HUDCalendar_H
+#define HUDCalendar_H
+
+#include "hudelement.h"
+
+#include <QString>
+#include <QStringList>
+#include <QRect>
+
+class HUDCalendarEntry: public HUDElement
+{
+    Q_OBJECT
+    QML_NAMED_ELEMENT(HUDCalendarEntry);
+
+    Q_PROPERTY(QString time MEMBER _time);
+    Q_PROPERTY(QString text MEMBER _text);
+
+public:
+    HUDCalendarEntry(QQuickItem *parent = nullptr);
+    ~HUDCalendarEntry();
+
+    int type() const override { return HUDElementType::CalendarEntry; }
+
+    void paint(QPainter* painter) override;
+    virtual void loadConfiguration(QJsonObject& json) override;
+    virtual void saveConfiguration(QJsonObject& json) override;
+
+protected:
+    QString _time;
+    QString _text;
+};
+
+
+class HUDCalendarDay : public HUDElement
+{
+    Q_OBJECT
+    QML_NAMED_ELEMENT(HUDCalendarDay);
+
+public:
+    HUDCalendarDay(QQuickItem *parent = nullptr);
+    ~HUDCalendarDay();
+
+    int type() const override { return HUDElementType::CalendarDay; }
+
+    void paint(QPainter* painter) override;
+    virtual void loadConfiguration(QJsonObject& json) override;
+    virtual void saveConfiguration(QJsonObject& json) override;
+
+private:
+
+};
+
+class HUDCalendar : public HUDElement
+{
+    Q_OBJECT
+    QML_NAMED_ELEMENT(HUDCalendar)
+
+    Q_PROPERTY(QString mode MEMBER _mode);
+
+public:
+    HUDCalendar(QQuickItem* parent = nullptr);
+    ~HUDCalendar();
+
+    int type() const override { return HUDElementType::Calendar; }
+
+    void paint(QPainter* painter) override;
+    virtual void loadConfiguration(QJsonObject& json) override;
+    virtual void saveConfiguration(QJsonObject& json) override;
+
+protected:
+    void calculateInternalGeometry();
+    void paintDay(QRect rect);
+
+protected slots:
+
+private:
+    QString _mode;			// possible values are: "day", "week", "month"
+    QList<int> hlines, vlines;		// starting coordinsates of horizontal and vertical lines
+    QRect carea;			// Calendar area where the days are 
+
+    
+
+};
+
+#endif
