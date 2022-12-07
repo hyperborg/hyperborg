9c86e15bca8bfc75c440de88423f77cd154c838c
Imre, Nagy <imi@hyperborg.com>
2022-12-07 17:10:08 +0100

    Adding Hungarian translation base

---
+++ build/vs2022/hyperborg/hyperborg__hu_HU.ts
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU">
+<context>
+    <name>HFS</name>
+    <message>
+        <location filename="../../../common/hfs.cpp" line="64"/>
+        <source>Section BOOTUP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../common/hfs.cpp" line="96"/>
+        <source>USING CONFIGURATION (BOOTUP) FILE: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../common/hfs.cpp" line="144"/>
+        <source>Configuration file &lt;%1&gt; cannot be opened!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../common/hfs.cpp" line="149"/>
+        <source>Config file is not defined in the bootup.ini file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../common/hfs.cpp" line="164"/>
+        <source>OPENING CONFIGURATION FILE &lt;%1&gt; with clear flag: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../common/hfs.cpp" line="245"/>
+        <source>JCHILD %1 is STRING  - set to %3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../common/hfs.cpp" line="556"/>
+        <source>Path &lt;%1&gt; cannot be created in HFS</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NodeCore</name>
+    <message>
+        <location filename="../../../node/nodecore.cpp" line="31"/>
+        <source> ============= PLUGIN INITALIZATION ==========================</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../node/nodecore.cpp" line="33"/>
+        <source>WebAssembly currently not supporting dynamic libraries(it can load modules though)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../node/nodecore.cpp" line="88"/>
+        <source>Plugin &lt;%1&gt; is found, but NOT inizialized since it is NOT ENABLED in the config</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../node/nodecore.cpp" line="99"/>
+        <source> ============= END OF PLUGIN INITALIZATION ====================</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>qmltest</name>
+    <message>
+        <location filename="../../../node/samples/qmltest.qml" line="24"/>
+        <source>HyperBorg QML Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
