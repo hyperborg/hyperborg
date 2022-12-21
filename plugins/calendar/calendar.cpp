644c6dc0e76d7a7ff4cfc52253902f42fc6f9f1f
Imre, Nagy <imi@hyperborg.com>
2022-12-21 21:41:11 +0100

    New Plugin: Calendar

---
+++ plugins/calendar/calendar.cpp
@@ -0,0 +1,15 @@
+#include <calendar.h>
+
+Calendar::Calendar(QObject *parent) : HyPluginInterface(), HyObject(parent)
+{
+    init();
+}
+
+Calendar::~Calendar()
+{
+}
+
+void Calendar::init()
+{
+}
+
---
+++ plugins/calendar/calendar.h
@@ -0,0 +1,34 @@
+#ifndef Calendar_H
+#define Calendar_H
+
+#include <QObject>
+#include <QtPlugin>
+#include <QString>
+#include <QTimer>
+#include <QDebug>
+#include <QStringList>
+
+#include "common.h"
+#include <hyplugin.h>
+#include <hyobject.h>
+
+class Calendar : public HyObject, public HyPluginInterface
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "com.nagyimre.HyperBorg.HyPluginInterface" FILE "calendar.json");
+    Q_INTERFACES(HyPluginInterface);
+
+public:
+    Calendar(QObject *parent=NULL);
+    ~Calendar();
+
+    QString name()          { return "calendar";                         		}
+    QString description()   { return "General calendar plugin";    			}
+    int implementation()    { return Developement;                      		}
+    QObject *getObject()    { return this;                              		}
+    QString author()        { return "Imre, Nagy  <i@hyperborg.com>";   		}
+
+    void init();
+
+};
+#endif
