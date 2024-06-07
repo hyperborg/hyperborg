05f99c95316f54586b8118023cb3e1968be1ff78
Imre,Nagy <imre.nagy@hyperborg.com>
2024-06-07 19:45:00 +0200

    Added I2C driver

---
+++ plugins/hb_i2c/hb_i2c.cpp
@@ -0,0 +1,14 @@
+#include <hb_i2c.h>
+#include "../../node/job.h"
+
+HB_I2C::HB_I2C(QObject* parent) : HyPluginInterface(), HyObject(parent)
+{
+}
+
+HB_I2C::~HB_I2C()
+{}
+
+
+void HB_I2C::init()
+{
+}
---
+++ plugins/hb_i2c/hb_i2c.h
@@ -0,0 +1,40 @@
+#ifndef hb_i2c_H
+#define hb_i2c_H
+
+#include <hyplugin.h>
+#include <hyobject.h>
+
+#include "common.h"
+#include "hfsitem.h"
+
+class Job;
+
+
+class HB_I2C : public HyObject, public HyPluginInterface
+{
+    Q_OBJECT
+        Q_PLUGIN_METADATA(IID "com.nagyimre.HyperBorg.HyPluginInterface" FILE "hb_i2c.json");
+    Q_INTERFACES(HyPluginInterface);
+
+public:
+    HB_I2C(QObject* parent = nullptr);
+    ~HB_I2C();
+
+    QString name() { return "HB_I2C"; }
+    QString description() { return "HB_I2C driver"; }
+    int implementation() { return Developement; }
+    QObject* getObject() { return this; }
+    QString author() { return "Imre, Nagy <i@hyperborg.com>"; }
+
+    void init();
+
+signals:
+
+public slots:
+
+private:
+    void clearDevices();
+
+};
+
+#endif
