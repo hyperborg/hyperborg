5ceff504670625e614125cf296b034c3b644231e
Imre, Nagy <imi@hyperborg.com>
2024-06-17 22:28:44 +0200

    DFR0971 sensor implementation

--- plugins/dfr0971/dfr0971_device.cpp
+++ plugins/dfr0971/dfr0971_device.cpp
@@ -1,12 +1,9 @@
-/* This file is moved here from HyperBorg testpad where a series of
-   Gravity 010V boards were tested. All contents are intentionally commented
-   out, since it is not yet compatible with HFS.
-*/
-
-/*
+#include "dfr0971_device.h"
+#include "../../node/job.h"
 
 #include <iostream>
 
+#if LINUX
 extern "C"
 {
     #include <linux/i2c-dev.h>
@@ -16,58 +13,96 @@ extern "C"
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
-
-int main(int argc, char **argv)
-{
-    printf("Hello world!");
-
-  int file;
-  int adapter_nr = 13;
-  char filename[20];
-
-  snprintf(filename, 19, "/dev/i2c-%d", adapter_nr);
-  file = open(filename, O_RDWR);
-  if (file < 0) {
-    printf("cannot open device\n");
-    // ERROR HANDLING; you can check errno to see what went wrong
-    exit(1);
-  }
-
-  int addr = 0x5f; // The I2C address
-
-  if (ioctl(file, I2C_SLAVE, addr) < 0) {
-    printf("cannot slave\n");
-       // ERROR HANDLING; you can check errno to see what went wrong
-    exit(1);
-  }
+#endif
 
 #define DAC_OUTRANGE uint8_t(0x01)
 #define CURRENT_REG uint8_t(0x02)
 #define CHANNEL_0 uint8_t(0x02)
 #define CHANNEL_1 uint8_t(0x04)
 
-    printf("setting 10V range\n");
-    __u8 a_values[1];
-    a_values[0] = 0x11; // should be 0 for 0-5V range
-    i2c_smbus_write_i2c_block_data(file, DAC_OUTRANGE, 1, (const __u8*)&a_values);
-
-    printf("setting channel 0 to ... \n");
-    __u8 b_values[2];
-    b_values[0]=0x00;
-    b_values[1]=0x01;
-    i2c_smbus_write_i2c_block_data(file, CHANNEL_0, 2, (const __u8*)&b_values);
-
-    __uint16_t voltage = 666;   // should be 10.00V
-    printf("setting channel 1 to ... %fV\n", voltage/100.0);
-    if (voltage>1000) voltage = 1000;
-    voltage = (__uint16_t)(((double)voltage/(double)1000.0)*4095.0);
-
-    voltage = voltage << 4;
-    i2c_smbus_write_i2c_block_data(file, CHANNEL_1, 2, (const __u8*)&voltage);
-
-    close(file);
-
-    return 0;
+DFR0971_Device::DFR0971_Device(QObject* parent) : HDevice(parent), file(-1), i2c_addr(0x5f)     // 0x5F is the default address for Gravity 0-10V
+{
+    setId("DFR0971DEV");
+    channel_reg[0] = CHANNEL_0;
+    channel_reg[1] = CHANNEL_1;
 }
 
-// CURRENT_REG 0x01 -> sets
+DFR0971_Device::~DFR0971_Device()
+{
+}
+
+void DFR0971_Device::init()
+{
+}
+
+bool DFR0971_Device::openBus()
+{
+#if LINUX
+    file = open(filename, O_RDWR);
+    if (file < 0) 
+    {
+        // printf("cannot open device\n");
+        // ERROR HANDLING; you can check errno to see what went wrong
+        return false
+    }
+
+    if (ioctl(file, I2C_SLAVE, i2c_addr) < 0) {
+        //printf("cannot slave\n");
+        // ERROR HANDLING; you can check errno to see what went wrong
+        return false;
+    }
+    return true;
+#else
+    return false;
+#endif
+}
+
+bool DFR0971_Device::closeBus()
+{
+#if LINUX
+    if (file>0)
+        close(file);
+    file = -1;
+    return true;
+#else
+    return false;
+#endif
+}
+
+QVariant DFR0971_Device::setDACRange(Job* job)
+{
+#if LINUX
+    if (openBus())
+    {
+        uint8_t value = 0x0;
+        int range = job->getAttribute("dac_range").toInt();
+        range = qBound(0, range, 10);
+
+        if (range == 10) value = 0x11;
+        i2c_smbus_write_i2c_block_data(file, DAC_OUTRANGE, 1, (const uint8_t*)&value);
+        closeBus();
+    }
+#endif
+    return QVariant();
+}
+
+QVariant DFR0971_Device::setVoltage(Job* job)
+{
+#if LINUX
+    if (openBus())
+    {
+        int channel = job->getAttribute("dac_channel").toInt();
+        double volt = job->getAttribute("dac_voltage").toDouble();
+        volt = qBound(0.0, volt, 10.0);
+
+        uint16_t voltage = volt;  
+        if (voltage > 1000) voltage = 1000;
+        voltage = (uint16_t)(((double)voltage / (double)1000.0) * 4096.0);
+        voltage = voltage << 4;
+        i2c_smbus_write_i2c_block_data(file, channel_reg[channel], 2, (const uint8_t*)&voltage);
+        closeBus();
+    }
+#endif
+    return QVariant();
+}
+
--- plugins/dfr0971/dfr0971_device.h
+++ plugins/dfr0971/dfr0971_device.h
@@ -1,4 +1,38 @@
 #ifndef DFR0971_DEVICE_H
 #define DFR0971_DEVICE_H
 
+#include "hdevice.h"
+#include "hfs_interface.h"
+#include "hfsitem.h"
+
+class Job;
+
+class DFR0971_Device : public HDevice
+{
+    Q_OBJECT
+public:
+    
+    DFR0971_Device(QObject* parent = nullptr);
+    ~DFR0971_Device();
+    HyObject::Type type() { return Device; }
+    void init();
+
+public slots:
+
+    QVariant setDACRange(Job* job);              // setting the 0-5V or 0-10V range for all channels
+    QVariant setVoltage(Job* job);              // set output voltage for a given channel
+
+private:
+    bool openBus();
+    bool closeBus();
+
+private:
+    QString i2c_filename;                       // name of the I2C bus file, like /dev/i2c-13
+    int     i2c_addr;                           // I2C register number. For the Gravity 010
+                                                // this should be in the 0x58-0x5F range 
+                                                // can be modified with the micro-dip switches on board
+    int     file;                               // I2C bus file handler
+    uint8_t channel_reg[2];
+};
+
 #endif
