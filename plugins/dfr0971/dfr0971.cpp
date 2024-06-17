e7e63c39fde384f5ddf371d0a0f0cfa2e82dc705
Imre,Nagy <imre.nagy@hyperborg.com>
2024-06-17 20:38:20 +0200

    Adding DFR0971 driver plugin

---
+++ plugins/dfr0971/dfr0971.cpp
@@ -0,0 +1,14 @@
+#include <dfr0971.h>
+#include "../../node/job.h"
+
+DFR0971::DFR0971(QObject* parent) : HyPluginInterface(), HyObject(parent)
+{
+}
+
+DFR0971::~DFR0971()
+{}
+
+
+void DFR0971::init()
+{
+}
---
+++ plugins/dfr0971/dfr0971.h
@@ -0,0 +1,40 @@
+#ifndef dfr0971_H
+#define dfr0971_H
+
+#include <hyplugin.h>
+#include <hyobject.h>
+
+#include "common.h"
+#include "hfsitem.h"
+
+class Job;
+
+
+class DFR0971 : public HyObject, public HyPluginInterface
+{
+    Q_OBJECT
+        Q_PLUGIN_METADATA(IID "com.nagyimre.HyperBorg.HyPluginInterface" FILE "dfr0971.json");
+    Q_INTERFACES(HyPluginInterface);
+
+public:
+    DFR0971(QObject* parent = nullptr);
+    ~DFR0971();
+
+    QString name() { return "DFR0971"; }
+    QString description() { return "DFR0971 driver"; }
+    int implementation() { return Developement; }
+    QObject* getObject() { return this; }
+    QString author() { return "Imre, Nagy <i@hyperborg.com>"; }
+
+    void init();
+
+signals:
+
+public slots:
+
+private:
+    void clearDevices();
+
+};
+
+#endif
